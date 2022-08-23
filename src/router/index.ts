73e765d5b9b7aca225d65ac7b4cfe31e0d71127d
lizezhi <lizezhi@jk366.net>
2022-08-23 09:35:58 +0800

    修改路由模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory} from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 import index from '../views/index.vue'
 const routes = [
     {
@@ -12,7 +12,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(), //替代之前的mode，是必须的
+    history: createWebHistory(), //替代之前的mode，是必须的
     routes
 });
 export default router;
