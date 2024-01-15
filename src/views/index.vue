ec5e51c0416b80c1d5447c35f4da2df1097e28a5
lizezhi <1>
2024-01-15 15:30:45 +0800

    test editor

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,33 +1,24 @@
 <template>
-  <el-upload
-      class="upload-demo"
-      drag
-      :with-credentials="true"
-      multiple
-      :on-change="change"
-      :auto-upload="false"
-  >
-    <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-    <div class="el-upload__text">
-      Drop file here or <em>click to upload</em>
-    </div>
-    <template #tip>
-      <div class="el-upload__tip">
-        jpg/png files with a size less than 500kb
-      </div>
-    </template>
-  </el-upload>
+  <div id="html"></div>
 </template>
 
 <script setup lang="ts">
-import { UploadFilled } from '@element-plus/icons-vue'
-import { upload } from '../api/test'
-const change = (e: any) => {
-  console.log(e);
-  let form = new window.FormData();
-  form.append('file', e.raw)
-  upload(form).then((res) => {
-    console.log(res);
+import * as monaco from 'monaco-editor';
+import { onMounted } from 'vue'
+onMounted(() => {
+  monaco.editor.create(document.getElementById('html')!, {
+    value: '<div>hello world</div>',
+    language: 'html',
+    theme: 'vs',
+    fontSize: 18,
+    automaticLayout: true,
   })
-}
+});
 </script>
+<style>
+#html {
+  width: 100%;
+  height: 300px;
+  border: 1px solid #ccc;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+
+const prefix = `monaco-editor/esm/vs`;
+
 export default defineConfig({
   plugins: [
     vue()],
   base: './',
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    jsonWorker: [`${prefix}/language/json/json.worker`],
+                    cssWorker: [`${prefix}/language/css/css.worker`],
+                    htmlWorker: [`${prefix}/language/html/html.worker`],
+                    tsWorker: [`${prefix}/language/typescript/ts.worker`],
+                    editorWorker: [`${prefix}/editor/editor.worker`],
+                },
+            },
+        },
+    },
   resolve: {
     // 别名
     alias: {
